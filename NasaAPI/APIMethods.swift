cb5658c6931c42017d2939b039f297968d3bad6d
Nick Viscomi <nicholasviscomi@gmail.com>
2020-09-20 17:01:52 -0400

    initial commit

---
+++ NasaAPI/APIMethods.swift
@@ -0,0 +1,19 @@
+//
+//  APIMethods.swift
+//  NasaAPI
+//
+//  Created by Nick Viscomi on 9/18/20.
+//  Copyright © 2020 Nick Viscomi. All rights reserved.
+//
+
+import UIKit
+
+class APOD {
+    
+}
+
+final class APIMethods {
+    public func getAPOD(date: String, completion: (APOD) -> Void) {
+        
+    }
+}
---
+++ NasaAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NasaAPI
+//
+//  Created by Nick Viscomi on 9/18/20.
+//  Copyright © 2020 Nick Viscomi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
